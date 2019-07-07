4e3e6b67032199ba18217427a59d12429a8fca5a
Xemon <biedukiewicz87@gmail.com>
2019-07-07 19:36:18 +0200

    Config has been commented

--- rank_prefix_system/sh_config.lua
+++ rank_prefix_system/sh_config.lua
@@ -1,12 +1,20 @@
-categories = {}
-categories[1] = "flota"
+categories = {} -- add to this table every ranks' category what's you want on your server!
+categories[1] = "army" -- example
+-- every next category must have higher index (2, 3, 4, 5...)
 
-prefixFactions = {}
-prefixFactions[FACTION_FLOTA] = "flota"
+prefixFactions = {} -- add individual factions to any category made before
+prefixFactions[FACTION_NAME] = "category" -- pattern
+prefixFactions[FACTION_ARMY] = "army" -- example
 
-ranksPrefix = {}
-ranksPrefix["flota"] = {
-    "Rekrut",
+ranksPrefix = {} -- this is table with every ranks' list
+ranksPrefix["name"] = { -- pattern
+    "Prefix1",
+    "Prefix2",
+    "Prefix3",
+    "Prefix4"
+}
+ranksPrefix["army"] = { -- example
+    "Recruit",
     "Officer",
     "Captain",
     "General",
